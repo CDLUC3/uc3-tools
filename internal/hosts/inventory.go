aac99e112d8d74813da45f5770a3ea39f1804d38
David Moles <david.moles@ucop.edu>
2019-02-11 16:21:02 -0800

    More on hosts

--- internal/hosts/inventory.go
+++ internal/hosts/inventory.go
@@ -5,6 +5,7 @@ package hosts
 
 type Inventory struct {
 	invPath string
+	hosts []HostRecord
 }
 
 func NewInventory(invPath string) *Inventory {
@@ -18,9 +19,24 @@ func (inv *Inventory) Print() error {
 	return nil
 }
 
+func (inv *Inventory) Hosts() ([]HostRecord, error) {
+	if inv.hosts == nil {
+		hosts, err := parseInvFile(inv.invPath)
+		if err != nil {
+			return nil, err
+		}
+		inv.hosts = hosts
+	}
+	return inv.hosts, nil
+}
+
 // ------------------------------
 // Unexported functions
 
 func (inv *Inventory) parse() error {
 	return nil
+}
+
+func parseInvFile(path string) ([]HostRecord, error) {
+	return nil, nil
 }
