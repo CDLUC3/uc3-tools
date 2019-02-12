ad918faafa9478cf74637e58a0e5c9b446016a05
David Moles <david.moles@ucop.edu>
2019-02-12 11:26:10 -0800

    Implemented inventory parsing, maybe

--- internal/hosts/host_record.go
+++ internal/hosts/host_record.go
@@ -1,7 +1,70 @@
 package hosts
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+const recordStartPrefix = "uc3,"
+const fqdnPrefix = "FQDN: "
+const cnamePattern = "^\\s +([a-z0-9.-]+cdlib.org)\\.\\s +CNAME.*Private"
+
+var cnameRe = regexp.MustCompile(cnamePattern)
+
 type HostRecord struct {
-	FQDN string
+	Service     string
+	Name        string
+	FQDN        string
 	Environment string
-	CNAMEs []string
+	CNAMEs      []string
 }
+
+func NewHostRecord(recordStartLine string) (*HostRecord, error) {
+	if !IsRecordStartLine(recordStartLine) {
+		return nil, fmt.Errorf("not a record start line: %#v", recordStartLine)
+	}
+	fields := strings.Split(recordStartLine, ",")
+	if len(fields) != 4 {
+		return nil, fmt.Errorf("can't parse record start %#v; expected 4 fields, got %d", line, len(fields))
+	}
+	env, svc, name := fields[0], fields[1], fields[2]
+	rec := HostRecord{
+		Environment: env,
+		Service:     svc,
+		Name:        name,
+	}
+	return &rec, nil
+}
+
+func (hr *HostRecord) AddFQDN(fqdnLine string) error {
+	if hr == nil {
+		return fmt.Errorf("can't add FQDN from %#v to nil record", fqdnLine)
+	}
+	if !IsFQDNLine(fqdnLine) {
+		return fmt.Errorf("not a FQDN line: %#v", fqdnLine)
+	}
+	hr.FQDN = strings.TrimPrefix(fqdnLine, fqdnPrefix)
+	return nil
+}
+
+func (hr *HostRecord) MaybeAddCname(line string) (bool, error) {
+	cnameMatch := cnameRe.FindStringSubmatch(line)
+	if cnameMatch == nil {
+		return false, nil
+	}
+	if hr == nil {
+		return false, fmt.Errorf("can't add CNAME from %#v to nil record", line)
+	}
+	hr.CNAMEs = append(hr.CNAMEs, cnameMatch[1])
+	return true, nil
+}
+
+func IsRecordStartLine(line string) bool {
+	return strings.HasPrefix(line, recordStartPrefix)
+}
+
+func IsFQDNLine(line string) bool {
+	return strings.HasPrefix(line, fqdnPrefix)
+}
+
--- internal/hosts/inventory.go
+++ internal/hosts/inventory.go
@@ -1,5 +1,12 @@
 package hosts
 
+import (
+	"bufio"
+	"os"
+	"sort"
+	"strings"
+)
+
 // ------------------------------------------------------------
 // Inventory
 
@@ -37,6 +44,60 @@ func (inv *Inventory) parse() error {
 	return nil
 }
 
+// TODO: extract a parsing struct
 func parseInvFile(path string) ([]HostRecord, error) {
-	return nil, nil
+	invFile, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		cerr := invFile.Close()
+		if err != nil {
+			err = cerr
+		}
+	}()
+
+	var recs []HostRecord
+	var current *HostRecord
+	scanner := bufio.NewScanner(invFile)
+	for scanner.Scan() {
+		line := scanner.Text()
+		isBlank := "" == strings.TrimSpace(line)
+		isRecordStart := IsRecordStartLine(line)
+		if isBlank || isRecordStart {
+			if current != nil {
+				sort.Strings(current.CNAMEs)
+				recs = append(recs, *current)
+				current = nil
+			}
+		}
+		if isBlank {
+			continue
+		}
+		if isRecordStart {
+			current, err = NewHostRecord(line)
+			if err != nil {
+				return nil, err
+			}
+			continue
+		}
+		isFqdn := IsFQDNLine(line)
+		if isFqdn {
+			err = current.AddFQDN(line)
+			if err != nil {
+				return nil, err
+			}
+			continue
+		}
+		_, err = current.MaybeAddCname(line)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if err = scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return recs, nil
 }
+
